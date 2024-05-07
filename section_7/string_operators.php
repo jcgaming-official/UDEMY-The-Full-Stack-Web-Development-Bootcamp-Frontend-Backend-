76478c48fb901f341a94a0d109a44a71f6c95abb
John Carl Quieta <quieta.johncarl158@gmail.com>
2024-05-07 19:17:17 +0800

    section 7 done

---
+++ section_7/string_operators.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Operator</title>
+</head>
+<body>
+    <?php
+    $fName = "John Carl ";
+    $lName = "Quieta";
+
+    echo $fName.$lName; //Concatenation
+echo "<br>";
+    $fName.=$lName; //Concatenation assignment
+    echo $fName; 
+    
+    ?>
+    
+</body>
+</html>
